6c36782a5013a7a1e355c6cb55f0cb241e11aee6
Juan Carlos <juan.andrade@televisatim.com>
2023-12-20 11:07:13 -0600

    #$MI$ Mirror feat(locales): Added colors and logo for destino 2024

--- packages/commons/src/themes/radio/index.js
+++ packages/commons/src/themes/radio/index.js
@@ -4,11 +4,28 @@ import {
   CARDINAL,
   POMEGRANATE,
 } from '../../utils/styled/constants';
+import { isPartOfDestino2024 } from '../../utils/header/data/locales/localesNavHelpers';
 import { getHorizontalThemeGradient } from '../../utils/styled/mixins';
 
 const exposedNavGradient = getHorizontalThemeGradient({ end: BLACK, start: BORDEAUX });
+/**
+ * Get the exposed nav background images for the header
+ * @param {*} data - Current data
+ * @returns {Object}
+ */
+const getExposedNavBackgroundImages = (data) => {
+  if (isPartOfDestino2024(data.uri)) {
+    return {
+      xxs: 'https://st1.uvnimg.com/d5/ac/ba0cd332476083d2bac9d6c8399b/background-expose-eleciones-national-mobile.png',
+      sm: 'https://st1.uvnimg.com/99/aa/9c9219064e6283684b9cd839ee54/background-expose-eleciones-national-tablet-768.png',
+      md: 'https://st1.uvnimg.com/d9/af/f4e030c443e69c1539b1bd3942f7/exposed-elections-national-md.png',
+      xl: 'https://st1.uvnimg.com/c8/97/c4f47b28479eb62b59ac0d1e219a/background-expose-eleciones-national-desktop.png',
+    };
+  }
+  return null;
+};
 
-export default () => ({
+export default (data = {}, options = {}) => ({
   card: {
     isDark: {
       audio: true,
@@ -29,6 +46,7 @@ export default () => ({
     },
   },
   exposedNavGradient,
+  exposedNavBackgroundImages: getExposedNavBackgroundImages(data),
   globalNavBackgroundColor: BLACK,
   headlineFont: {
     rectangle: 'uvs-font-a-bold',
