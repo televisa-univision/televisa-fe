e3c5647aea347666c4c212bb7759694492e78db5
Juan Carlos <juan.andrade@televisatim.com>
2023-12-20 11:07:17 -0600

    #$MI$ Mirror feat(locales): Added colors and logo for destino 2024

--- packages/commons/src/utils/header/data/radio/index.js
+++ packages/commons/src/utils/header/data/radio/index.js
@@ -1,6 +1,6 @@
 import { toRelativeUrl } from '../../../helpers';
 import { getBrandable } from '../../helpers';
-
+import { isPartOfDestino2024 } from '../locales/localesNavHelpers';
 import genericNavData from '../genericNavData';
 import radioLinks from './links/radio';
 import getRadioStationData from './getRadioStationData';
@@ -17,9 +17,24 @@ const uforiaLogo = 'https://st1.uvnimg.com/75/8c/e912578d40d7a12842b8c8cb59e6/lo
 
 // Radio stations data file
 export default (data = {}) => {
-  const uri = data?.uri ?? '/';
   const defaultNav = genericNavData(data);
   const brandable = getBrandable(data);
+
+  if (isPartOfDestino2024(data.uri)) {
+    return {
+      ...defaultNav,
+      title: {
+        link: brandable.uri,
+        logo: 'https://st1.uvnimg.com/2f/db/d7d5845c4e6f9d89971e636d1363/destino-2024-wide-199x31px.svg',
+        name: null,
+        target: '_self',
+        maxWidth: '300px',
+        maxHeight: '48px',
+      },
+    };
+  }
+
+  const uri = data?.uri ?? '/';
   const relativeUri = toRelativeUrl(uri);
   const isRadioSection = radioSections.some(section => relativeUri.startsWith(section));
   const title = {
