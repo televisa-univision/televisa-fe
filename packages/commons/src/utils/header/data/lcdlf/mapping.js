0857db60a34f3d869e1c19661dcda46f207bdc45
juanandradeb <144062963+juanandradeb@users.noreply.github.com>
2023-12-20 12:42:43 -0600

    #$MI$ Mirror feat(lcdlf): Site setup (#10025)\n\n* feat(lcdlf): Site setup\r\n\r\n* feat(amp): serverless configuration

---
+++ packages/commons/src/utils/header/data/lcdlf/mapping.js
@@ -0,0 +1,8 @@
+import * as pageCategories from '../../../../constants/pageCategories';
+import index from '.';
+
+export default {
+  [pageCategories.LCDLF]: index,
+  [pageCategories.LCDLF_SHOW]: index,
+  [pageCategories.LCDLF_VOTACIONES]: index,
+};
