2a0e98d3ee928ef258459363073990adb7d6c561
Marcelo Retana <marcelo.retana@3pillarglobal.com>
2023-12-20 16:11:14 -0600

    #$MI$ Mirror Update PR

--- packages/app/src/components/base/Error404/Error404.spec.js
+++ packages/app/src/components/base/Error404/Error404.spec.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { mount } from 'enzyme';
-import { Provider } from 'react-redux';
 import { act } from 'react-dom/test-utils';
+import { Provider } from 'react-redux';
 
 // eslint-disable-next-line no-restricted-imports
 import configureStore from '@univision/fe-commons/dist/store/configureStore';
@@ -10,20 +10,6 @@ import Error404 from '.';
 import Search from '../../contentTypes/Search';
 
 const store = configureStore();
-
-/**
- * Wait for async behaviours to finish
- * @param {Object} wrapper component
- * @param {function} _actions any actions to be triggered
- * @returns {Promise<void>}
- */
-const actions = async (wrapper, _actions) => {
-  await act(async () => {
-    await (new Promise(resolve => setTimeout(resolve, 0)));
-    if (_actions) _actions();
-    wrapper.update();
-  });
-};
 
 describe('Error404 tests', () => {
   let props;
@@ -45,7 +31,9 @@ describe('Error404 tests', () => {
       </Provider>,
     );
 
-    await actions(wrapper);
+    await act(async () => {
+      await new Promise(resolve => setImmediate(resolve));
+    });
 
     expect(wrapper.exists()).toBe(true);
     expect(wrapper.find(Search)).toHaveLength(1);
