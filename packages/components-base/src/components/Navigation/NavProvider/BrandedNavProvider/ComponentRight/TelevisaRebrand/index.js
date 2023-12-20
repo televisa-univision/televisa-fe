a8c5138d34244f3e327e8e9d809eb48c4c7c6ea9
juanandradeb <144062963+juanandradeb@users.noreply.github.com>
2023-12-20 15:21:05 -0600

    #$MI$ Mirror fix(losbingers): DCENGA-3808 QA fixes (#10014)

--- packages/components-base/src/components/Navigation/NavProvider/BrandedNavProvider/ComponentRight/TelevisaRebrand/index.js
+++ packages/components-base/src/components/Navigation/NavProvider/BrandedNavProvider/ComponentRight/TelevisaRebrand/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
@@ -7,6 +8,7 @@ import Icon from '@univision/fe-icons/dist/components/Icon';
 import Tracker from '@univision/fe-commons/dist/utils/tracking/tealium/Tracker';
 import { useSelector } from 'react-redux';
 import { themeSelector } from '@univision/fe-commons/dist/store/selectors/page-selectors';
+import { VIX_BANNER_DOMAIN, VIX_BANNER_PATH } from '@univision/fe-commons/dist/constants/televisaSitesData';
 
 // eslint-disable-next-line import/no-named-as-default, import/no-named-as-default-member
 import Styles from './TelevisaRebrand.styles';
@@ -39,23 +41,37 @@ const TelevisaRebrand = () => {
     if (typeof process.env.ENVIRONMENT !== 'undefined') {
       window.open('/search', '_self');
     } else {
-      window.open(`${window.location.href}/search`, '_self');
+      window.open(`${window.location.href.replace('/search', '')}/search`, '_self');
     }
   };
 
+  /**
+   * Handle click event
+   * @param {Object} e - event
+   */
+  const vixClickHandler = (e) => {
+    e.preventDefault();
+    Tracker.fireEvent({
+      event: 'topnav-vix-logo-header',
+      destination_url: `${VIX_BANNER_DOMAIN}${VIX_BANNER_PATH}`,
+    });
+    window.open(`${VIX_BANNER_DOMAIN}${VIX_BANNER_PATH}`, '_self');
+  };
+
   return (
-    // eslint-disable-next-line jsx-a11y/anchor-is-valid
-    <Link onClick={handleClick}>
-      <Wrapper>
+    <Wrapper>
+      <Link onClick={handleClick}>
         <SearchIcon>
           <Icon name="search" width={32} height={32} fill={fillIcon} className={'labelSearch'} />
           <SearchLabel>{localization.get('search')}</SearchLabel>
         </SearchIcon>
+      </Link>
+      <Link onClick={vixClickHandler}>
         <VixLogoWrapper>
           <Icon name="vix" viewBox="0 0 90 80" width={40} height={40} fill={fillIcon} />
         </VixLogoWrapper>
-      </Wrapper>
-    </Link>
+      </Link>
+    </Wrapper>
   );
 };
 
