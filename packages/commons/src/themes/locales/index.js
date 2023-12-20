e7edcdd2c9764748d10612ed375e723cdbbce9d8
Juan Carlos <juan.andrade@televisatim.com>
2023-12-20 11:07:12 -0600

    #$MI$ Mirror feat(locales): Added colors and logo for destino 2024

--- packages/commons/src/themes/locales/index.js
+++ packages/commons/src/themes/locales/index.js
@@ -7,8 +7,31 @@ import {
   LIGHT_BLUE,
   LOCAL_GRADIENT,
 } from '@univision/fe-utilities/styled/constants';
+import { isPartOfDestino2024 } from '../../utils/header/data/locales/localesNavHelpers';
 
-export default () => ({
+/**
+ * Get the exposed nav background images for the header
+ * @param {*} data - Current data
+ * @returns {Object}
+ */
+const getExposedNavBackgroundImages = (data) => {
+  if (isPartOfDestino2024(data.uri)) {
+    return {
+      xxs: 'https://st1.uvnimg.com/d5/ac/ba0cd332476083d2bac9d6c8399b/background-expose-eleciones-national-mobile.png',
+      sm: 'https://st1.uvnimg.com/99/aa/9c9219064e6283684b9cd839ee54/background-expose-eleciones-national-tablet-768.png',
+      md: 'https://st1.uvnimg.com/d9/af/f4e030c443e69c1539b1bd3942f7/exposed-elections-national-md.png',
+      xl: 'https://st1.uvnimg.com/c8/97/c4f47b28479eb62b59ac0d1e219a/background-expose-eleciones-national-desktop.png',
+    };
+  }
+  return {
+    xxs: 'https://st1.uvnimg.com/47/ba/62d9e25e4961803713176c5b39a2/mobile414.jpg',
+    sm: 'https://st1.uvnimg.com/88/b6/393155704acbb5edb6b4191ce52a/tablet768.jpg',
+    md: 'https://st1.uvnimg.com/db/f9/fffea358440ba906967a24562592/desktop1024.jpg',
+    xl: 'https://st1.uvnimg.com/88/e1/e50d52cf4535831ea913e9311ea4/desktop1440.jpg',
+  };
+};
+
+export default (data = {}, options = {}) => ({
   card: {
     isDark: {
       horoscopos: true,
@@ -19,12 +42,7 @@ export default () => ({
   gradient: LOCAL_GRADIENT,
   globalNavBackgroundColor: EBONY,
   isBrandedHeaderBlack: false,
-  exposedNavBackgroundImages: {
-    xxs: 'https://st1.uvnimg.com/47/ba/62d9e25e4961803713176c5b39a2/mobile414.jpg',
-    sm: 'https://st1.uvnimg.com/88/b6/393155704acbb5edb6b4191ce52a/tablet768.jpg',
-    md: 'https://st1.uvnimg.com/db/f9/fffea358440ba906967a24562592/desktop1024.jpg',
-    xl: 'https://st1.uvnimg.com/88/e1/e50d52cf4535831ea913e9311ea4/desktop1440.jpg',
-  },
+  exposedNavBackgroundImages: getExposedNavBackgroundImages(data),
   primary: ASTRONAUT,
   secondary: HAITI,
   custom: {
