58c62f6a47df305b6ef1b2b18c086c716bdb9938
juanandradeb <144062963+juanandradeb@users.noreply.github.com>
2023-12-20 15:21:00 -0600

    #$MI$ Mirror fix(losbingers): DCENGA-3808 QA fixes (#10014)

--- packages/app/src/components/AMP/Header/logos.js
+++ packages/app/src/components/AMP/Header/logos.js
@@ -21,6 +21,7 @@ import lasestrellasLogo from '@univision/fe-commons/dist/assets/images/logo-las-
 import elnu9veLogo from '@univision/fe-commons/dist/assets/images/logo-elnu9ve-horizontal-solid.svg';
 import telehitLogo from '@univision/fe-commons/dist/assets/images/logo_telehit_horizontal_solid.svg';
 import distritocomediaLogo from '@univision/fe-commons/dist/assets/images/logo_distritocomedia_horizontal_solid.svg';
+import losbingersLogo from '@univision/fe-commons/dist/assets/images/losbingers-logo-color.svg';
 import lcdlfLogo from '@univision/fe-commons/dist/assets/images/lcdlf-logo-horizontal_solid.png';
 
 export const logos = {
@@ -100,6 +101,12 @@ export const logos = {
     width: 144,
     height: 29,
   },
+  losbingers: {
+    src: losbingersLogo,
+    variant: 'dark',
+    width: 144,
+    height: 29,
+  },
   lcdlf: {
     src: lcdlfLogo,
     variant: 'dark',
@@ -129,6 +136,7 @@ export default (pageData, pageCategory, siteName) => {
     [categories.UNICABLE]: logos.unicable,
     [categories.DISTRITO_COMEDIA]: logos.distritocomedia,
     [categories.ELNU9VE]: logos.elnu9ve,
+    [categories.losbingers]: logos.losbingers,
     [categories.LCDLF]: logos.lcdlf,
   };
 
